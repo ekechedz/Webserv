19cd9ac31fa4847dab1e4b22963a7a45bafd1c66
Canteen43 <karl.weihmann@gmail.com>
2025-05-11 15:13:49 +0200

    improved status codes

--- include/Response.hpp
+++ include/Response.hpp
@@ -5,18 +5,20 @@
 #include <sstream>
 
 
+
+
 class Response {
 private:
 	int _statusCode;
-	std::string _statusMessage;
 	std::map<std::string, std::string> _headers;
 	std::string _body;
 
 public:
 	Response();
-	void setStatus(int code, const std::string& message);
+	void setStatus(int code);
 	void setHeader(const std::string& key, const std::string& value);
 	void setBody(const std::string& body);
 	std::string toString() const;
 };
 
+const char* getReasonPhrase(int code);
--- src/Response.cpp
+++ src/Response.cpp
@@ -1,10 +1,9 @@
 #include "../include/Response.hpp"
 
-Response::Response() : _statusCode(200), _statusMessage("OK") {}
+Response::Response() : _statusCode(0) {}
 
-void Response::setStatus(int code, const std::string& message) {
+void Response::setStatus(int code) {
 	_statusCode = code;
-	_statusMessage = message;
 }
 
 void Response::setHeader(const std::string& key, const std::string& value) {
@@ -17,7 +16,7 @@ void Response::setBody(const std::string& body) {
 
 std::string Response::toString() const {
 	std::ostringstream response;
-	response << "HTTP/1.1 " << _statusCode << " " << _statusMessage << "\r\n";
+	response << "HTTP/1.1 " << _statusCode << " " << getReasonPhrase(_statusCode) << "\r\n";
 	for (std::map<std::string, std::string>::const_iterator it = _headers.begin(); it != _headers.end(); ++it) {
 		response << it->first << ": " << it->second << "\r\n";
 	}
@@ -25,4 +24,41 @@ std::string Response::toString() const {
 	response << "\r\n";
 	response << _body;
 	return response.str();
+}
+
+#define HTTP_STATUS_CODES \
+    /* Successful (2xx): */ \
+    X(200, "OK") \
+    X(201, "Created") \
+    X(204, "No Content") \
+    /* Redirection (3xx): */ \
+    X(301, "Moved Permanently") \
+    X(302, "Found") \
+    X(303, "See Other") \
+    X(307, "Temporary Redirect") \
+    X(308, "Permanent Redirect") \
+    /* Client Errors (4xx): */ \
+    X(400, "Bad Request") \
+    X(403, "Forbidden") \
+    X(404, "Not Found") \
+    X(405, "Method Not Allowed") \
+    X(413, "Payload Too Large") \
+    X(415, "Unsupported Media Type") \
+    X(418, "I'm a teapot") \
+    X(429, "Too Many Requests") \
+    /* Server Errors (5xx): */ \
+    X(500, "Internal Server Error") \
+    X(501, "Not Implemented") \
+    X(502, "Bad Gateway") \
+    X(503, "Service Unavailable") \
+    X(504, "Gateway Timeout")
+
+
+const char* getReasonPhrase(int code) {
+    switch (code) {
+        #define X(num, text) case num: return text;
+        HTTP_STATUS_CODES
+        #undef X
+        default: return "Unknown Status";
+    }
 }
