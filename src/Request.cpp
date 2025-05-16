4fdad42c426846df835f62f8b2fdf1c4ff1348a1
Elmira <ekechedz@student.42berlin.de>
2025-05-16 11:42:04 +0200

    added setters and getters for request

--- src/Request.cpp
+++ src/Request.cpp
@@ -1,5 +1,22 @@
 #include "../include/Server.hpp"
 #include "../include/Request.hpp"
+#include <cstdlib>
+
+Request::Request() : matchedLocation(NULL){}
+
+std::string Request::getMethod() const { return method; }
+std::string Request::getPath() const { return path; }
+std::string Request::getProtocol() const { return protocol; }
+std::map<std::string, std::string> Request::getHeaders() const { return headers; }
+std::string Request::getBody() const { return body; }
+const LocationConfig* Request::getMatchedLocation() const { return matchedLocation; }
+
+void Request::setMethod(const std::string& m) { method = m; }
+void Request::setPath(const std::string& p) { path = p; }
+void Request::setProtocol(const std::string& pr) { protocol = pr; }
+void Request::setHeaders(const std::map<std::string, std::string>& h) { headers = h; }
+void Request::setBody(const std::string& b) { body = b; }
+void Request::setMatchedLocation(const LocationConfig* loc) { matchedLocation = loc; }
 
 Request parseHttpRequest(const std::string &rawRequest)
 {
@@ -12,9 +29,14 @@ Request parseHttpRequest(const std::string &rawRequest)
 		throw std::runtime_error("Invalid HTTP request line");
 
 	std::istringstream requestLine(line);
-	requestLine >> request.method >> request.path >> request.protocol;
+	std::string method, path, protocol;
+	requestLine >> method >> path >> protocol;
+	request.setMethod(method);
+	request.setPath(path);
+	request.setProtocol(protocol);
 
 	// Parse headers
+	std::map<std::string, std::string> headers;
 	while (std::getline(stream, line) && line != "\r")
 	{
 		if (!line.empty() && line[line.size() - 1] == '\r')
@@ -25,48 +47,50 @@ Request parseHttpRequest(const std::string &rawRequest)
 			std::string key = line.substr(0, colon);
 			std::string value = line.substr(colon + 1);
 			value.erase(0, value.find_first_not_of(" \t"));
-			request.headers[key] = value;
+			headers[key] = value;
 		}
 	}
-	if (request.headers.count("Transfer-Encoding") &&
-		request.headers["Transfer-Encoding"] == "chunked")
-		request.body = decodeChunkedBody(stream);
-	else if (request.headers.count("Content-Length"))
+	request.setHeaders(headers);
+
+	if (headers.count("Transfer-Encoding") &&
+		headers["Transfer-Encoding"] == "chunked")
+		request.setBody(decodeChunkedBody(stream));
+	else if (headers.count("Content-Length"))
 	{
-		int length = std::atoi(request.headers["Content-Length"].c_str());
+		int length = std::atoi(headers["Content-Length"].c_str());
 		std::string body(length, '\0');
 		stream.read(&body[0], length);
-		request.body = body;
+		request.setBody(body);
 	}
 
 
 
-	std::cout << "Received HTTP request from client " << ": "
-			  << request.method << " " << request.path << " " << request.protocol << "\n";
+	std::cout << "Received HTTP request from client: "
+			  << request.getMethod() << " "
+			  << request.getPath() << " "
+			  << request.getProtocol() << "\n";
 	return request;
 }
-
 void Request::print() const
 {
 	std::cout << "\n====== HTTP Request ======" << std::endl;
 
-	std::cout << "Method   : " << method << std::endl;
-	std::cout << "Path     : " << path << std::endl;
-	std::cout << "Protocol : " << protocol << std::endl;
+	std::cout << "Method   : " << getMethod() << std::endl;
+	std::cout << "Path     : " << getPath() << std::endl;
+	std::cout << "Protocol : " << getProtocol() << std::endl;
 
 	std::cout << "\nHeaders:" << std::endl;
-	std::map<std::string, std::string>::const_iterator it;
-	for (it = headers.begin(); it != headers.end(); ++it)
+	std::map<std::string, std::string> hdrs = getHeaders();
+	for (std::map<std::string, std::string>::const_iterator it = hdrs.begin(); it != hdrs.end(); ++it)
 		std::cout << "  " << it->first << ": " << it->second << std::endl;
 
 	std::cout << "\nBody:" << std::endl;
-	if (body.empty())
+	if (getBody().empty())
 		std::cout << "  (empty)" << std::endl;
 	else
-		std::cout << body << std::endl;
+		std::cout << getBody() << std::endl;
 
-	std::cout << "===========================\n"
-			  << std::endl;
+	std::cout << "===========================\n" << std::endl;
 }
 
 
